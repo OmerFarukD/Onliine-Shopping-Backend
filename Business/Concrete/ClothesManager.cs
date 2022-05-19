a1695270fb015dfc41a811bfa005d9a4e0b6e1f0
Omer Faruk Dogan <omer.crlys.44@gmail.com>
2022-05-19 20:10:17 +0300

    Cache and RemoveCache Aspects integrated

--- Business/Concrete/ClothesManager.cs
+++ Business/Concrete/ClothesManager.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Caching;
 using Core.Aspects.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -21,35 +22,41 @@ namespace Business.Concrete
         {
             _clothesDal = clothesDal;
         }
+        [CacheAspect]
         public IDataResult<List<Clothes>> GetAll()
         {
             return new SuccessDataResult<List<Clothes>>(_clothesDal.GetAll());
         }
 
         [ValidationAspect(typeof(ClothesValidator))]
+        [CacheRemoveAspect("IClothesService.Get")]
         public IResult Add(Clothes clothes)
         {
             _clothesDal.Delete(clothes);
             return new SuccessResult(Messages.ClothesAddMessage);
         }
 
+        [CacheRemoveAspect("IClothesService.Get")]
         public IResult Delete(Clothes clothes)
         {
             _clothesDal.Delete(clothes);
             return new SuccessResult(Messages.ClothesDeleteMessage);
         }
 
+        [CacheRemoveAspect("IClothesService.Get")]
         public IResult Update(Clothes clothes)
         {
             _clothesDal.Update(clothes);
             return new SuccessResult(Messages.ClothesUpdateMessage);
         }
 
+        [CacheAspect]
         public IDataResult<List<Clothes>> GetAllByStartsWith(string pattern)
         {
             return new SuccessDataResult<List<Clothes>>(_clothesDal.GetAll(c => c.ClothesName.StartsWith(pattern)));
         }
 
+        [CacheAspect]
         public IDataResult<Clothes> GetById(long id)
         {
             return new SuccessDataResult<Clothes>(_clothesDal.Get(c => c.Id == id));
